05447d52252822ccaa49579ddeb72d154606297f
huynh hoang <huynhhoang712003@gmail.com>
2026-01-12 15:53:13 +0700

    Thêm món ăn

--- src/app/manage/accounts/add-employee.tsx
+++ src/app/manage/accounts/add-employee.tsx
@@ -84,7 +84,15 @@ export default function AddEmployee() {
     }
   };
   return (
-    <Dialog onOpenChange={setOpen} open={open}>
+    <Dialog
+      onOpenChange={(value) => {
+        if (!value) {
+          reset();
+        }
+        setOpen(value);
+      }}
+      open={open}
+    >
       <DialogTrigger asChild>
         <Button size="sm" className="h-7 gap-1">
           <PlusCircle className="h-3.5 w-3.5" />
