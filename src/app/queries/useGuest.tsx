f0da7bae1cc6fa81d61b0909d2f070512fa91745
huynh hoang <huynhhoang712003@gmail.com>
2026-01-14 18:39:10 +0700

    Khai báo API Order và hiển thị menu cho khách gọi món

--- src/app/queries/useGuest.tsx
+++ src/app/queries/useGuest.tsx
@@ -1,5 +1,5 @@
 import guestApiRequest from "@/apiRequests/guest";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const useLoginMutation = () => {
   return useMutation({
@@ -10,5 +10,18 @@ export const useLoginMutation = () => {
 export const useLogoutMutation = () => {
   return useMutation({
     mutationFn: guestApiRequest.logout,
+  });
+};
+
+export const useGuestOrderMutation = () => {
+  return useMutation({
+    mutationFn: guestApiRequest.order,
+  });
+};
+
+export const useGuestGetOrderListMutation = () => {
+  return useQuery({
+    queryKey: ["guest-orders"],
+    queryFn: guestApiRequest.getOrderList,
   });
 };
